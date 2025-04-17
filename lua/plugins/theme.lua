8270a72ee96d8e64b4f48c598677fe1c1726535a
CallOrRet <CallOrRet@outlook.com>
2025-04-18 03:18:19 +0800

    update

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -8,8 +8,9 @@ return {
     config = function()
       vim.g.moonflyWinSeparator = 2
       vim.cmd([[colorscheme moonfly]])
+
       vim.api.nvim_set_hl(0, "Type", { link = "MoonflyOrange" })
-      vim.api.nvim_set_hl(0, "@type.builtin", { link = "MoonflyGreen" })
+      -- vim.api.nvim_set_hl(0, "@type.builtin", { link = "MoonflyGreen" })
     end,
   },
 }
