5c1856fcd0ee9f5632df797d2a668f33efd8d6fb
CallOrRet <CallOrRet@outlook.com>
2025-04-18 19:33:31 +0800

    udpate

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,8 +1,13 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.g.snacks_animate = false
+
 local opt = vim.opt
 opt.mouse = ""
-opt.shell = "bash"
+opt.shell = "fish"
 opt.swapfile = false
+
+local env = vim.fn.environ()
+vim.env.PATH = "/root/lang/go/bin:/root/lang/node/bin:/root/lang/rust/bin" .. env.PATH
+
+vim.g.snacks_animate = false
