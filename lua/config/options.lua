4f1cf69eb1eeab2174c8ca0bc560c82e94c9f46d
CallOrRet <CallOrRet@outlook.com>
2025-04-19 11:58:39 +0800

    update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,11 +3,19 @@
 -- Add any additional options here
 
 local opt = vim.opt
+
 opt.mouse = ""
 opt.shell = "fish"
 opt.swapfile = false
 
-local env = vim.fn.environ()
-vim.env.PATH = "/root/lang/go/bin:/root/lang/node/bin:/root/lang/rust/bin:" .. env.PATH
-
 vim.g.snacks_animate = false
+
+local lang_dir = "/root/lang"
+local lang_dirs = vim.split(vim.fn.glob(lang_dir .. "/*"), "\n", { trimempty = true })
+for _, dir in pairs(lang_dirs) do
+  local bin_path = dir .. "/bin"
+  local stat = vim.uv.fs_stat(bin_path)
+  if stat and stat.type == "directory" then
+    vim.env.PATH = bin_path .. ":" .. vim.env.PATH
+  end
+end
