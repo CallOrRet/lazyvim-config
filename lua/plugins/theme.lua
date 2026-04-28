4a9c8fc04ff6209c4f3f3459c7feaee2c7998fdc
CallOrRet <CallOrRet@icloud.com>
2026-04-28 20:20:34 +0800

    update

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -3,11 +3,5 @@ return {
     "bluz71/vim-moonfly-colors",
     name = "moonfly",
     lazy = true,
-    config = function()
-      vim.schedule(function()
-        vim.api.nvim_set_hl(0, "Type", { link = "MoonflyOrange" })
-        vim.api.nvim_set_hl(0, "@type.builtin", { link = "Type" })
-      end)
-    end,
   },
 }
